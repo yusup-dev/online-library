e3922a939f9bf3c5a6c14533f52bfd88da2ad920
Muhamad Yusup <mhmdy5p0317@gmail.com>
2024-10-23 10:33:58 +0700

    first commit

--- src/main/java/com/onlinelibrary/service/impl/AuthServiceImpl.java
+++ src/main/java/com/onlinelibrary/service/impl/AuthServiceImpl.java
@@ -19,7 +19,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.nio.file.attribute.UserPrincipal;
 import java.util.HashSet;
 import java.util.Set;
 
